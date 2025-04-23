382770b8700fdd45d1096acd743aa35fa427177c
Kayla Ngo <kngo29@csu.fullerton.edu>
2025-04-23 01:04:04 -0700

    updating branch from Fontend_design

--- pages_app/urls.py
+++ pages_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path("", views.home, name="home"),
   path("checkers/", views.checkers, name="checkers"),
+  path("chess/", views.chess, name="chess"),
 ]
--- pages_app/views.py
+++ pages_app/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-  return render(request, "home.html")
+    return render(request, "home.html")
 
 def checkers(request):
-  return render(request, "index.html")
+    return render(request, "index.html")
+
+def chess(request):
+    return render(request, "chess.html")
