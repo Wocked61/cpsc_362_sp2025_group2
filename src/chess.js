999d7e0ca4bb7e3bd28afb4531181fa12a19f6b4
Kevin Chau <kchau1205@csu.fullerton.edu>
2025-05-02 17:39:19 -0700

    added template for chess piece theme, only classic for now

--- src/chess.js
+++ src/chess.js
@@ -237,6 +237,26 @@ function createColorPickers() {
     applyInitialBoardColors();
   });
 
+  const pieceThemes = {
+    classic: {
+      name: "Classic",
+      styles: `
+        .piece.white.pawn { background-image: url('https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg'); }
+        .piece.white.rook { background-image: url('https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg'); }
+        .piece.white.knight { background-image: url('https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg'); }
+        .piece.white.bishop { background-image: url('https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg'); }
+        .piece.white.queen { background-image: url('https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg'); }
+        .piece.white.king { background-image: url('https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg'); }
+        .piece.black.pawn { background-image: url('https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg'); }
+        .piece.black.rook { background-image: url('https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg'); }
+        .piece.black.knight { background-image: url('https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg'); }
+        .piece.black.bishop { background-image: url('https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg'); }
+        .piece.black.queen { background-image: url('https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg'); }
+        .piece.black.king { background-image: url('https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg'); }
+      `
+    },
+};
+
 function setupBoard() {
     boardContainer.innerHTML = "";
     for (let row = 0; row < rows; row++) {
